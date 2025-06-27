0dca63547e855a84415228dc4f26a0171c0bb205
Fariaragnar <jorgefaria.log@outlook.com>
2025-06-27 20:54:35 -0300

    atualização

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,18 +1,20 @@
+import java.util.Locale;
 import java.util.Scanner;
 
 public class ContaTerminal {
 
     public static void main(String[] args) throws Exception {
-        Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
         // Exibir as mensagens para o nosso usuário
+        System.out.println("Digite o Usuário: ");
         int usuario = scanner.nextInt();
-        System.out.printf("Digite o Usuário: %d \n", usuario);
+        scanner.nextLine();
+        System.out.println("Digite sua Agência: ");
         String agencia = scanner.nextLine();
-        System.out.printf("Digite sua Agência: %s \n", agencia);
+        System.out.println("Digite seu nome:  ");
         String nomeCliente = scanner.nextLine();
-        System.out.printf("Digite seu nome: $s \n", nomeCliente);
-        Double saldoDepositado = scanner.nextDouble();
-        System.out.printf("Deposite um valor: %d \n", saldoDepositado);
+        System.out.println("Deposite um valor:  ");
+        Float saldoDepositado = scanner.nextFloat();
         // Obter pela scanner os valores digitados no terminal
         System.out.printf("Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, \n conta %d e seu saldo %.2f já está disponível para saque.", nomeCliente, agencia, usuario, saldoDepositado);
         // Exibir a mensagem conta criada
